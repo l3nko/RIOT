d8fd60a74c97558eb187abef84125aa77173353a
Niccolò Chiti <niccolo.chiti@gmail.com>
2015-05-19 20:05:55 +0200

    fix relic integration

--- pkg/relic/relic-build/build.sh
+++ pkg/relic/relic-build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ls |grep -v build.sh |xargs rm -r
-COMP="-g -m32" LINK="-m32" cmake -DARCH=X86 -DSEED=ZERO -DWORD=32 -DWITH="ALL" -DDCMAKE_INSTALL_PREFIX=/Users/magenta/Desktop/l3nko-RIOT/pkg/relic/relic-build/ ../relic-src/
+COMP="-g -m32" LINK="-m32" cmake -DARCH=X86 -DWORD=32 -DDEBUG=on -DSEED=ZERO -DWORD=32 -DWITH="ALL" -DCMAKE_INSTALL_PREFIX:PATH=.  ../relic-src/
